0ee0a85b1d5527138aa4d50c55a573a5dd0f39f8
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-09-24 21:15:34 -0300

    aula-comentarios-em-JS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("hello World")
 console.log("bora começar essa aula")
+
+// escrever bara-barra vc comenta uma linha de codigo
+
+/* 
+ja o barra-* é para fazer um comentário de multiplas linhas
+onde vc pode usar essa ferramenta de comentário para:
+- esconder do navegador partes do codigo
+- comentar breviamente como lembrete oq aquela parte 
+especifica do codigo faz
+- ou usar como um jeito de organizar sua linha de pensamento 
+*/

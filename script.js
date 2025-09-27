c3fae343baf1acd2f49988bd33d7a299b209f0ce
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-09-27 17:38:37 -0300

    undefined-e-null

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// Aula sobre variaveis tipo boolean
+// Aula sobre variaveis tipo undefined e null
 /* 
  */
+ 
+//undefined (idefinido) e null (nulo)
 
-let is_loading = true
-let isLoading= false
-console.log (is_loading)
-console.log (typeof is_loading)
-console.log (isLoading)
-console.log (typeof isLoading)
+let teste1 
+console.log("o valor é:", teste1)
+
+let teste2 = null
+console.log("o valor é:", teste2)
+
+
+

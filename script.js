1733f96d0539725cb7ba6f0305ee3813af0f8781
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-09-27 17:15:30 -0300

    template-literals

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Aula sobre tipos de variaveis (string)
+// Aula sobre template literals
 /* 
-    O tipo de variavel string pode ser mostrado 
-    com aspas duplas ("")
-    com aspas simples famoso apóstofo ('')
-    e com o acento grave (``) que permite escrever 
-    com tabulação
-    segue os exemplos 
+    no JS é possivel concatenar texto e/ou usar comando no 
+    meio do texto.
  */
-let user_name = ("Raphael")
-console.log (typeof user_name)
 
+    let user = "Raphael"
+    let email = "RaphaelSteimvacher2251@gmail.com"
+    let age = "24"
 
-console.log ("texto com aspas simples 'no meio do texto")
-//console.log (" Não é possivel fazer assim "pois o codigo se perder", tendeu ")
+    // colocar mais de um parametro deve usar , no texto
 
-console.log('O contrário é "tambem" possivel')
-//console.log ('assim como o mesmo 'não é' possivel')
- console.log(`
-    E por fim se necessário um texto com tabulação
-    podemos usar o acento grave para isso.
-    `)
+    console.log (user, email, age, "teste")
 
+    //concatenar texto tambem é possivel 
+
+    let message = "olá, " + user + ". Vc conectou ao servidor com o email " + email
+
+    console.log (message)
+
+    // ou podemos usar o comando ${} para utilizar dentro do console.log ficando mais organizado.
+
+    console.log (`
+        Olá, ${user}.
+        vc conectou ao servidor com o email ${email}
+        `)
 

ab65a73cd8288600665912baa7d4e3422d4879ba
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-09-27 17:33:47 -0300

    tipos-de-variaveis-boolean

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-// Aula sobre variaveis tipo number
+// Aula sobre variaveis tipo boolean
 /* 
  */
 
-//number e string
-
-console.log (5)
-console.log (typeof 5)
-console.log ('5')
-console.log (typeof "5")
-
-//Numero positivo 
-
-console.log (5)
-
-//Inteiro negativo
-
-console.log (-5)
-
-//inteiro real ou float
-
-console.log (225.1)
-console.log (125,1) //padrão na progamação utilizar ponto e não virgula
-
-//Nan = Not a number 
-
-console.log (5 / "raphael")
-
-
-
+let is_loading = true
+let isLoading= false
+console.log (is_loading)
+console.log (typeof is_loading)
+console.log (isLoading)
+console.log (typeof isLoading)

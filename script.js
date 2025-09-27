975a2805885745966f915aff0d18b123f10e2e4f
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-09-27 16:06:46 -0300

    Aula-Scope-hosting

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-/* O java scriprt tem 3 jeitos de de declarar uma variavel
-sendo esses:
-- var que é um tipo de variavél que pode ser sobrescrita
--let que é um tipo de varivel que eu não posso sobreescrever
-e constat 
-*/
-let user = "raphael"
-console.log (user)
+// Aula sobre scope e hosting
+/* var é mais usado para declaração de variaveis globais
+enquanto o let e const são mais utilizados para declaração
+de variaveis em blocos.
+A separação de global e blocos é feita com {    }, onde 
+fora das chaves é o scope global e dentro da chaves é o scope
+do bloco.   */
 
-var email = "raphaelsteimvacher2251@gmail.com"
-console.log(email)
+var user = "raphael"
+let email = "criptoalien2251@gmail.com"
 
-const number = "2251"
-console.log(number)
+console.log (email)
+{
+    let email = "raphaelSteimvacher2251@gmail.com"
+    let age = "24"
+    let adreess = "av. sueo haguihara"
+    const number = "50"
+    console.log (user)
+    console.log (email)
+    console.log (age)
+    console.log (adreess)
+    console.log (number)
+}
+
+console.log (email)
+
+
+
